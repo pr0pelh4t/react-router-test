b6d5fd5c3a19ad44c6ea45e116a2120d3dd454da
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2022-08-10 15:44:29 +0300

    fix link text

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open xml view
         </a>
       </header>
     </div>
